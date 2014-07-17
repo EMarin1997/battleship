faeaf6cd7ca69eb869a411e3f7fe82ca1eded8b4
Marin <Emarin1997@gmail.com>
2014-07-17 13:00:12 -0700

    generates enemy ship location without using  a list

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,8 +3,8 @@ class Board < ActiveRecord::Base
 	before_create :random
 
 	def random
-		point = ["A1","A2","A3","A4","A5","A6","B1","B2","B3","B4","B5","B6","C1","C2","C3","C4","C5","C6","D1","D2","D3","D4","D5","D6","E1","E2","E3","E4","E5","E6","F1","F2","F3","F4","F5","F6"]
-		self.enemy = point[rand(point.length)]
+		@size = 10
+		self.enemy = "#{("A".ord+rand(@size)).chr}#{rand(@size)}"
 	end
 
 	def test(x)
